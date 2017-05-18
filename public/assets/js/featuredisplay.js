4d8b47419eef4a6fce2e5f4736223fe39b08fd21
shivamneeraj23 <shivamneeraj23@gmail.com>
2017-05-18 15:54:16 +0530

    Unecessary Files Removed

--- public/assets/js/featuredisplay.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('featureTable', []);
-app.controller('featureDisplay', function($scope, $http) {
-    $http.get("http://localhost:8080/display")
-    .then(function (response) {$scope.names = response.data.records;});
-});
